eae6000af8cba7d787849993c0282b63e8e72289
Daniel Estrada <daestradam@unal.edu.co>
2023-03-31 02:51:00 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\SellerController;
 
 Route::apiResource('sellers', SellerController::class);
 
+//Route::apiResource('sellers', SellerController::class)->except(['create', 'edit']);
+
 /*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
