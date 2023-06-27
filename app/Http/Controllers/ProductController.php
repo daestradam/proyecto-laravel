36d21179e5a9176d01609d74fb12a726f1b11005
Daniel Estrada <daestradam@unal.edu.co>
2023-06-27 01:33:40 -0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Services\Products\ProductService;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
+use App\DataTransferObjects\Product\StoreProductData;
 
 class ProductController extends Controller
 {
+    public function __construct(protected ProductService $productService)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +25,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return new ProductCollection($this->productService->index());
     }
 
     /**
@@ -34,26 +44,31 @@ class ProductController extends Controller
      * @param  \App\Http\Requests\StoreProductRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreProductRequest $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+
+        return response()->json(
+            new ProductResource(
+                $this->productService->store(StoreProductData::from($request))
+            )
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Product  $seller
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show(int $id)
     {
-        //
+        return response()->json($this->productService->show($id));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Product  $seller
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -65,22 +80,22 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \App\Http\Requests\UpdateProductRequest  $request
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Product  $seller
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(Request $request, int $id)
     {
-        //
+        return response()->json($this->productService->update($request, $id));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(int $id)
     {
-        //
+        return response()->json($this->productService->destroy($id));
     }
 }
